fb32ea94a8fec486d07717ae7ae802efcccada7b
정현희 <wjdgusgml997@naver.com>
2020-11-25 20:25:03 +0900

    [+]ADD Login and Signup Form

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="moveTo">회원가입</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    moveTo() {
+      this.$emit("changeComponents", "Signup");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/Signup.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <label for="userId">아이디</label>
+    <input type="text" v-model="userId" @blur="checkID" />
+    <p v-if="error" style="color: red">{{ error }}</p>
+    <label for="userPassword">비밀번호</label>
+    <input type="password" v-model="userPassword" />
+    <label for="userPasswordCheck">비밀번호확인</label>
+    <input type="password" v-model="userPasswordCheck" />
+    <label for="userName">이름</label>
+    <input type="text" v-model="userName" />
+  </div>
+</template>
+
+<script>
+import SERVER from "@/api/url";
+import axios from "axios";
+export default {
+  data() {
+    return {
+      userId: "",
+      userPassword: "",
+      userPasswordCheck: "",
+      userName: "",
+      error: "",
+    };
+  },
+  methods: {
+    checkID() {
+      if (this.userId.length == 0) {
+        this.error = "아이디는 필수입니다.";
+      } else {
+        axios
+          .get(SERVER.URL + SERVER.ROUTES.SIGNUP, {
+            user_id: this.userId,
+          })
+          .then((res) => {
+              console.log(res)
+          });
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
